1b03b4b3cc861c40212857b2ae0e65f186def4d8
cgonz4476 <cgonz4476@gmail.com>
2016-01-06 09:02:25 -0800

    Signed-off-by: cgonz4476 <cgonz4476@gmail.com>

---
+++ projects & etc/militsry budget calculation/main.cpp
@@ -0,0 +1,28 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on January 6, 2016, 8:13 AM
+ */
+
+#include <iostream>
+using namespace std;
+
+ 
+
+//Global Constant
+const char cnvPerc=100;
+
+//function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+//Declare and initialize variables
+    float milBgt/fedBdgt*cnvPerc;
+    
+    //Output the results
+    cout<<"The Military Budget =$"<<milBdgt"<<endl;
+    cout<<"The Federal Budget = "<<fedBdgt"<<endl;
+    return 0;
+}
+
